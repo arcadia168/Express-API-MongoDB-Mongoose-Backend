8d7272562e3099c1dee5a51881db55f499b9b76a
Erin Eland <erin@erineland.com>
2015-02-18 00:45:52 +0000

    making changes to server

--- frontend/YesGetIn/www/js/controllers.js
+++ frontend/YesGetIn/www/js/controllers.js
@@ -10,12 +10,37 @@ angular.module('starter.controllers', [])
 })
 
 .controller('RoundsCtrl', function($scope, Rounds) {
+    
+  //only publicly accessible elements get added to the scope
   $scope.rounds = Rounds.all();
   $scope.remove = function(round) {
     Rounds.remove(round);
   }
+      
+  //private array to hold the predictions
+  var _predictions = [
+      {
+        id: 1,
+        fixtureid: 1,
+        prediction: 'homewin'
+      },
+      {
+        id: 2,
+        fixtureid: 2,
+        prediction: 'draw'
+      }
+  ];
+    
   $scope.predict = function(roundid, prediction) { //prediction will be left, right, up
-      Rounds.predict(roundid, prediction);
+    Rounds.predict(roundid, prediction);
+  }
+  //home win on swipe left
+  $scope.onSwipeLeft(fixtureid){
+     //call the function in the service
+  }
+  // away win on swipe right
+  $scope.onSwipeRight(fixtureid){
+    //call the function in the service
   }
 })
 

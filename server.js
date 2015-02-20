a78096ab2852d7a09cd6f91ce500d95e131f254b
Erin Eland <erin@erineland.com>
2015-02-20 14:13:45 +0000

    Attempting to get server working in some form or other

--- server.js
+++ server.js
@@ -2,140 +2,148 @@
 
 var PORT = process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT  || 8080;
 var IPADDRESS = process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-
+//
+//var express = require('express');
+//var fs      = require('fs');
+//var mongoose = require('mongoose');
+//var app = express();
+//
+//mongoose.connect('mongodb://localhost/nodejs');
+//var db = mongoose.connection;
+//db.on('error', function() {
+//  throw new Error('Unable to connect to database');
+//});
+//app.configure(function() {
+//  app.use(express.bodyParser());
+//});
+//
+//app.get('/', function(req, res) {
+//
+//    // Some fake testing data
+//    var rounds = [{
+//        id: 0,
+//        name: 'Round 1',
+//        description: 'Here are the fixtures for Round 1',
+//        fixtures: [
+//            { id: 1,
+//                name: 'Fixture 1',
+//                description: 'Them vs. Those'
+//            },
+//            { id: 2,
+//                name: 'Fixture 2',
+//                description: 'Them vs. Those'
+//            },
+//            { id: 3,
+//                name: 'Fixture 3',
+//                description: 'Them vs. Those'
+//            },
+//            { id: 4,
+//                name: 'Fixture 4 ',
+//                description: 'Them vs. Those'
+//            }
+//        ]
+//    }, {
+//        id: 1,
+//        name: 'Round 2',
+//        description: 'Here are the fixtures for Round 2',
+//        fixtures: [
+//            { id: 1,
+//                name: 'Fixture 1',
+//                description: 'Them vs. Those'
+//            },
+//            { id: 2,
+//                name: 'Fixture 2',
+//                description: 'Them vs. Those'
+//            },
+//            { id: 3,
+//                name: 'Fixture 3',
+//                description: 'Them vs. Those'
+//            },
+//            { id: 4,
+//                name: 'Fixture 4 ',
+//                description: 'Them vs. Those'
+//            }
+//        ]
+//    }, {
+//        id: 2,
+//        name: 'Round 3',
+//        description: 'Here are the fixtures for Round 3',
+//        fixtures: [
+//            { id: 1,
+//                name: 'Fixture 1',
+//                description: 'Them vs. Those'
+//            },
+//            { id: 2,
+//                name: 'Fixture 2',
+//                description: 'Them vs. Those'
+//            },
+//            { id: 3,
+//                name: 'Fixture 3',
+//                description: 'Them vs. Those'
+//            },
+//            { id: 4,
+//                name: 'Fixture 4 ',
+//                description: 'Them vs. Those'
+//            }
+//        ]
+//    }, {
+//        id: 3,
+//        name: 'Round 4',
+//        description: 'Here are the fixtures for Round 4',
+//        fixtures: [
+//            { id: 1,
+//                name: 'Fixture 1',
+//                description: 'Them vs. Those'
+//            },
+//            { id: 2,
+//                name: 'Fixture 2',
+//                description: 'Them vs. Those'
+//            },
+//            { id: 3,
+//                name: 'Fixture 3',
+//                description: 'Them vs. Those'
+//            },
+//            { id: 4,
+//                name: 'Fixture 4 ',
+//                description: 'Them vs. Those'
+//            }
+//        ]
+//    }, {
+//        id: 4,
+//        name: 'Round 5',
+//        description: 'Here are the fixtures for Round 5',
+//        fixtures: [
+//            { id: 1,
+//                name: 'Fixture 1',
+//                description: 'Them vs. Those'
+//            },
+//            { id: 2,
+//                name: 'Fixture 2',
+//                description: 'Them vs. Those'
+//            },
+//            { id: 3,
+//                name: 'Fixture 3',
+//                description: 'Them vs. Those'
+//            },
+//            { id: 4,
+//                name: 'Fixture 4 ',
+//                description: 'Them vs. Those'
+//            }
+//        ]
+//    }];
+//
+//    res.send(rounds);
+//});
+//
+//app.listen(PORT);
+//
+//
+//
 var express = require('express');
-var fs      = require('fs');
-var mongoose = require('mongoose');
 var app = express();
 
-mongoose.connect('mongodb://localhost/nodejs');
-var db = mongoose.connection;
-db.on('error', function() {
-  throw new Error('Unable to connect to database');
-});
-app.configure(function() {
-  app.use(express.bodyParser());
+app.get('/', function (req, res) {
+    res.send('Hello World!')
 });
 
-app.get('/', function(req, res) {
-
-    // Some fake testing data
-    var rounds = [{
-        id: 0,
-        name: 'Round 1',
-        description: 'Here are the fixtures for Round 1',
-        fixtures: [
-            { id: 1,
-                name: 'Fixture 1',
-                description: 'Them vs. Those'
-            },
-            { id: 2,
-                name: 'Fixture 2',
-                description: 'Them vs. Those'
-            },
-            { id: 3,
-                name: 'Fixture 3',
-                description: 'Them vs. Those'
-            },
-            { id: 4,
-                name: 'Fixture 4 ',
-                description: 'Them vs. Those'
-            }
-        ]
-    }, {
-        id: 1,
-        name: 'Round 2',
-        description: 'Here are the fixtures for Round 2',
-        fixtures: [
-            { id: 1,
-                name: 'Fixture 1',
-                description: 'Them vs. Those'
-            },
-            { id: 2,
-                name: 'Fixture 2',
-                description: 'Them vs. Those'
-            },
-            { id: 3,
-                name: 'Fixture 3',
-                description: 'Them vs. Those'
-            },
-            { id: 4,
-                name: 'Fixture 4 ',
-                description: 'Them vs. Those'
-            }
-        ]
-    }, {
-        id: 2,
-        name: 'Round 3',
-        description: 'Here are the fixtures for Round 3',
-        fixtures: [
-            { id: 1,
-                name: 'Fixture 1',
-                description: 'Them vs. Those'
-            },
-            { id: 2,
-                name: 'Fixture 2',
-                description: 'Them vs. Those'
-            },
-            { id: 3,
-                name: 'Fixture 3',
-                description: 'Them vs. Those'
-            },
-            { id: 4,
-                name: 'Fixture 4 ',
-                description: 'Them vs. Those'
-            }
-        ]
-    }, {
-        id: 3,
-        name: 'Round 4',
-        description: 'Here are the fixtures for Round 4',
-        fixtures: [
-            { id: 1,
-                name: 'Fixture 1',
-                description: 'Them vs. Those'
-            },
-            { id: 2,
-                name: 'Fixture 2',
-                description: 'Them vs. Those'
-            },
-            { id: 3,
-                name: 'Fixture 3',
-                description: 'Them vs. Those'
-            },
-            { id: 4,
-                name: 'Fixture 4 ',
-                description: 'Them vs. Those'
-            }
-        ]
-    }, {
-        id: 4,
-        name: 'Round 5',
-        description: 'Here are the fixtures for Round 5',
-        fixtures: [
-            { id: 1,
-                name: 'Fixture 1',
-                description: 'Them vs. Those'
-            },
-            { id: 2,
-                name: 'Fixture 2',
-                description: 'Them vs. Those'
-            },
-            { id: 3,
-                name: 'Fixture 3',
-                description: 'Them vs. Those'
-            },
-            { id: 4,
-                name: 'Fixture 4 ',
-                description: 'Them vs. Those'
-            }
-        ]
-    }];
-
-    res.send(rounds);
-});
-
-app.listen(8080);
-
-
-
+var server = app.listen(PORT);

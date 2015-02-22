79a329f4a1ae7d0407daa4b51086f293ccb4988a
danielcattlin <daniecattlin@hotmail.co.uk>
2015-02-22 21:35:31 +0000

    finished functions

--- mongo/controllers/users.js
+++ mongo/controllers/users.js
@@ -36,15 +36,40 @@ exports.addPredictions = function(req, res) {
 
 exports.getPredictions = function(req, res) {
   var username = req.params.username;
+  User.find({'username': username}, 'predictions', function(err, results) {
+    res.send(results);
+  });
 };
 
 exports.updatePrediction = function(req, res) {
   var username = req.params.username;
+  User.findOneAndUpdate({'username': username,
+                        'predictions.fixture': {"$ne": req.body.fixture}},
+                        { $set: {'predictions.$': req.body}}
+                        {upsert : true},
+                        function(err, number) {
+    if(err) return console.log(err);
+    return res.send(202);
+  });
 };
 
 exports.clearPredictions = function(req, res) {
   User.update({}, {$pull: {'predictions': {}}}, function(err, number) {
     if(err) return console.log(err);
     return res.send(202);
+  });
+};
+
+exports.resetPredictions = function(req, res) {
+  var username = req.params.username;
+  User.update({'username': username}, {$pull: {'predictions': {}}}, function(err, number) {
+    if(err) return console.log(err);
+    User.update({'username': username}, { $pushAll: {'predictions': req.body}},
+      {safe: true, upsert: false},
+      function(err, number) {
+        if(err) return console.log(err);
+        return res.send(202);
+      }
+    );
   });
 };

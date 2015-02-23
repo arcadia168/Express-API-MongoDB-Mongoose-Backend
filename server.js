9225b601c84a4b792e1a94ca4e41a7c0692b77d7
Erin Eland <erin@erineland.com>
2015-02-23 13:46:12 +0000

    Enable CORS on server and updating the rounds tab.:

--- server.js
+++ server.js
@@ -30,6 +30,13 @@ db.on('error', function () {
   throw new Error('unable to connect to database at mongodb://localhost/nodejs');
 });
 
+//try to enable cors
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(cors());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
